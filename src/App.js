9aab423677f129a19050dcffea613ed28057d693
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2025-05-15 11:26:52 +0600

    OKey

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-44 h-44 bg-yellow-300"></div>
+      <div className="text-4xl bg-green-700">ALI AKBER MEHEDI</div>
+      <div className="text-4xl bg-green-700">ALI AKBER MEHEDI</div>
+    </div>
+  );
+}
+
+export default App;
